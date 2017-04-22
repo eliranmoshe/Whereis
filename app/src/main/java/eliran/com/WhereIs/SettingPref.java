6f202907e39ac1e352a7f998686770169f16a788
eliranmoshe <kaza.moshe@gmail.com>
2017-04-22 13:40:33 +0300

    work neer by and by city need to change km to miles

---
+++ app/src/main/java/eliran/com/WhereIs/SettingPref.java
@@ -0,0 +1,17 @@
+package eliran.com.WhereIs;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by eliran on 4/22/2017.
+ */
+
+public class SettingPref extends PreferenceActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
